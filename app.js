5335ce27139ac60c21ebfe5e91efa5477bdfdce8
zychzygaz <78308744+zychzygaz@users.noreply.github.com>
2021-03-08 15:53:19 +0100

    media queries + calendar

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-
-
-
 function setter(){
-    fullDate = new Date()
-
-    console.log(fullDate);
-
+    let fullDate = new Date()
+    // Clock
     h = fullDate.getHours()
     m = fullDate.getMinutes()
     s = fullDate.getSeconds()
-    console.log(h, m, s);
+    // console.log(h, m, s);
     if (h < 10){
         h = "0"+ h
     }
-    document.getElementById("h").innerHTML = h;
+    document.getElementById("hour").innerHTML = h;
     if (m < 10){
         m = "0"+ m
     }
@@ -22,6 +17,28 @@ function setter(){
         s = "0"+ s
     }
     document.getElementById("sec").innerHTML = ":" + s;
+
+    // Date
+    day = fullDate.getDate();
+    month = fullDate.getMonth();
+    year = fullDate.getFullYear();
+    month += 1
+
+    if (day < 10){
+        day = "0"+ day
+    }
+    document.getElementById("day").innerHTML = day;
+    if (month < 10){
+        month = "0"+ month
+    }
+    document.getElementById("month").innerHTML = ":" + month;
+    if (year < 10){
+        year = "0"+ year
+    }
+    document.getElementById("year").innerHTML = ":" + year;
+
+    console.log(day, month, year);
+
 }
 
 
